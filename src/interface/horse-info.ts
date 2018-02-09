37e4818ae9b9f439468c24204e832b15a2413aa5
linyimin <linyimin520812@gmail.com>
2018-02-09 14:01:27 +0800

    Add horse information interface

---
+++ src/interface/horse-info.ts
@@ -0,0 +1,13 @@
+export interface HorseInfo{
+    name: String,           // 名称
+    title: String,          // 头衔
+    age: string,            // 年龄
+    level: String,          // 水平
+    gender: String,         // 性别
+    pedigree: String,       // 血统簿
+    color: String,          // 颜色
+    withers: String,        // 马肩隆
+    price: Number,          // 价格
+    isXrayCheck: Boolean,   // X光检查通过
+    isDiagnosis: Boolean    // 通过诊断
+} 
