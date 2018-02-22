39e964d86a958596583070582eb35a809f0da677
linyimin <linyimin520812@gmail.com>
2018-02-22 18:11:56 +0800

    Add router to type Router

--- src/router/user.ts
+++ src/router/user.ts
@@ -2,7 +2,8 @@ import * as express from "express";
 import {signin} from "../middlewares/user/signin";
 import {signup} from "../middlewares/user/signup";
 import {signout} from "../middlewares/user/signout";
-const router = express.Router();
+import { Router } from "express";
+const router:Router = express.Router();
 // 登录
 router.post("/signin", signin);
 // 注册
