52d6eef6282f9b96663436c3d4c3dbae603ba795
lym152898 <15289842383@163.com>
2018-02-26 20:08:46 +0800

    Add isExist interface

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
 import {signin} from "../middlewares/user/signin";
-import {signup} from "../middlewares/user/signup";
+import {signup, isExist} from "../middlewares/user/signup";
 import {signout} from "../middlewares/user/signout";
 import { Router } from "express";
 const router:Router = express.Router();
+// 检查用户名是否存在
+router.post("/isExist", isExist);
 // 登录
 router.post("/signin", signin);
 // 注册
