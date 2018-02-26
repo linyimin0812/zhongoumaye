04ba5c24e17a69476be297e9e047c5f9020b4ca1
lym152898 <15289842383@163.com>
2018-02-26 20:07:43 +0800

    Change req.query to req.body and put hash into function

--- src/middlewares/user/signin.ts
+++ src/middlewares/user/signin.ts
@@ -2,13 +2,19 @@ import { User } from "../../interface/user-list";
 import { UserList } from "../../models/user-list";
 import * as crypto from "crypto";
 import { GeneralResult } from "../../interface/general-result";
-const hash = crypto.createHash('sha256');
 function signin(req, res) {
     if(req.session.user){
-        return res.json(req.session.user)
+        let returnModel: GeneralResult = {
+            result: true,
+            reason: null,
+            data: req.session.user
+        }
+        return res.json(returnModel)
     }
-    let username: string = req.query.username;
-    let password: string = req.query.password;
+    let username: string = req.body.username;
+    let password: string = req.body.password;
+
+    let hash = crypto.createHash('sha256');
     UserList.findByUsername(username).then(function (result) {
         if (result && result.length > 0) {
             let rawpassword: string = hash.update(password).digest('hex');
