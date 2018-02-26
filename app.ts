51773db122f3ad860c27252e5da2669d48368f71
lym152898 <15289842383@163.com>
2018-02-26 20:01:44 +0800

    Add express static resource

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import * as mongoose from 'mongoose';
 import {config} from "./config/default";
 import * as express from "express";
 import * as session from "express-session";
+import * as bodyParser from 'body-parser';
 import {router} from "./src/router/user";
 // 链接数据库
 mongoose.connect(config.database.mongodb);
@@ -10,10 +11,15 @@ mongoose.connection.on("error", (error) => {
 });
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 app.use(session(config.session));
 app.use("/user", router);
 
 app.listen(8001, function(){
     console.log("listen port 8001");
-})
-
+});
